4e2950075530d9371ce28fd58a0f2711d45064e9
Florian Heinemann <florian.heinemann@gmail.com>
2014-04-23 14:50:45 +0200

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+	res.render('index');
+};
+
+exports.create = function(req, res){
+	// TODO: Create storage
+	// TODO: Display 
+	res.redirect('/');
+};
